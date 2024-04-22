5d8bae6cee2faaf90051e465986dae9987a2a020
mathcarvalhom <mathcarvalhomp@gmail.com>
2024-04-21 23:18:45 -0400

    first-commit

---
+++ Primeiro-procedimento/PessoaFisicaRepo.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.util.ArrayList;
+import java.io.*;
+import java.util.List;
+
+public class PessoaFisicaRepo {
+    private ArrayList<PessoaFisica> pessoasFisicas = new ArrayList<>();
+
+    public void inserir(PessoaFisica pessoaFisica) {
+        pessoasFisicas.add(pessoaFisica);
+    }
+
+    public void alterar(PessoaFisica pessoaFisica) {
+        for (int i = 0; i < pessoasFisicas.size(); i++) {
+            if (pessoasFisicas.get(i).getId() == pessoaFisica.getId()) {
+                pessoasFisicas.set(i, pessoaFisica);
+                return;
+            }
+        }
+    }
+
+    public void excluir(int id) {
+        pessoasFisicas.removeIf(p -> p.getId() == id);
+    }
+
+    public PessoaFisica obter(int id) {
+        return pessoasFisicas.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<PessoaFisica> obterTodos() {
+        return new ArrayList<>(pessoasFisicas);
+    }
+
+    public void persistir(String nomeArquivo) throws IOException {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(nomeArquivo))) {
+            out.writeObject(pessoasFisicas);
+        }
+    }
+
+    public void recuperar(String nomeArquivo) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(nomeArquivo))) {
+            pessoasFisicas = (ArrayList<PessoaFisica>) in.readObject();
+        }
+    }
+}
